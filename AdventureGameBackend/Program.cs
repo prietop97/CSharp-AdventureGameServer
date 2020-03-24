8893abd15afe0c52dcbd4ce9ae3be29af2bf5e2f
Pedro Prieto <pedro.prieto003@gmail.com>
2020-03-24 18:25:41 -0400

    Server up, boiler plate

---
+++ AdventureGameBackend/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AdventureGameBackend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Game Server";
+
+            Server.Start(50, 26950);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AdventureGameBackend/Server.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace AdventureGameBackend
+{
+    class Server
+    {
+        // Max Player Allowed in the server
+        public static int MaxPlayers { get; private set; }
+
+        // Port for the server to listen on
+        public static int Port { get; private set; }
+
+        // Dictionary of all the clients connections
+        public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
+
+
+        // Socket Connection (From System.Net.Sockets)
+        public static TcpListener tcpListener;
+        public static void Start(int _maxPlayers, int _port)
+        {
+            MaxPlayers = _maxPlayers;
+
+            Port = _port;
+
+            Console.WriteLine($"Server starting.");
+            InitializeServerData();
+            // Creating the socket
+            tcpListener = new TcpListener(IPAddress.Any, Port);
+            tcpListener.Start();
+
+            // Starts a loop of accepting tcp clients or players
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+
+            Console.WriteLine($"Server started on {Port}.");
+
+        }
+
+        private static void TCPConnectCallback(IAsyncResult _result)
+        {
+            TcpClient _client = tcpListener.EndAcceptTcpClient(_result);
+            Console.WriteLine(_client);
+            
+            // Starts async function again in case we get a new one
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+
+            Console.WriteLine($"Incoming connection from {_client.Client.RemoteEndPoint}...");
+
+
+            // Gets the next available client/playerwhere stream havent been started yet and assigns that to the new connection
+            for (int i = 1; i <= MaxPlayers; i++)
+            {
+                if (clients[i].tcp.socket == null)
+                {
+                    clients[i].tcp.Connect(_client);
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{_client.Client.RemoteEndPoint} failed  to connect: Server full");
+        }
+
+        // Creates all players of the clients and store them for easy referencing, stream has not started yet
+        private static void InitializeServerData()
+        {
+            for(int i = 1; i <= MaxPlayers; i++)
+            {
+                clients.Add(i,new Client(i));
+            }
+        }
+    }
+}
